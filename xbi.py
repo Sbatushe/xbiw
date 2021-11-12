bdd9386c866ef2518f6431d77b13377fbb199059
Emanuele Sabato <44072381+Sbatushe@users.noreply.github.com>
2021-11-12 13:11:14 +0100

    Add files via upload

---
+++ xbi.py
@@ -0,0 +1,52 @@
+# xbps-install wrapper that doesn't care about case sensitive or package versions
+# Prerequisites: doas, fish
+# Written by Sbatushe
+
+import sys
+import os
+import subprocess
+import re
+
+if (len(sys.argv)>1):
+
+    #grub xbps-query output
+    rv = subprocess.Popen(["xbps-query","-Rs",sys.argv[1]],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+    stdout,stderr = rv.communicate()
+    if stderr != "None":
+    
+        #scan xbps-query output
+        pkglist = []
+        columns = re.split(r'\\n',str(stdout))
+        for row in columns:
+            element = re.split(r' ',row)
+            if len(element)>1:
+                pkglist.append(element[1])
+
+        #scan alternatives and find package to install
+        candidates = []
+        if (len(pkglist)>0):
+            for pkg in pkglist:
+                req_components = re.split(r'-',sys.argv[1])
+                pkg_components = re.split(r'-',pkg)
+                if (len(req_components) == (len(pkg_components)-1)):
+                    flag = True
+                    for n in range(len(pkg_components)-1):
+                        if req_components[n].lower() != pkg_components[n].lower():
+                            flag = False
+                    if (flag):
+                        candidates.append(pkg)
+
+        #install package
+        if (len(candidates)== 1):
+            command = "doas xbps-install "+candidates[0]
+            print("\033[22;92m* \033[22;97m"+ command)
+            os.system(command)
+                
+        else:
+            print("\033[22;91mE \033[22;97mPackage not found")
+    else:
+        print("\033[22;91mE \033[22;97mAn error occurred")
+else:
+    print("\033[22;91mE \033[22;97mYou need to specify a package name")
